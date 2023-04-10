3050b5a52383b6189b23462210b561c0e41700be
SupBlizard <58865670+SupBlizard@users.noreply.github.com>
2023-04-10 15:01:28 +0100

    Cleanup background.go and prepare for batching #19

--- background.go
+++ background.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 
 	"github.com/faiface/pixel"
 )
 
 const STAR_MAX_PHASE int = 5
+const STAR_DISTANCE float64 = 65
+const STAR_RANDOMNESS int = 60
+const STAR_SIZE float64 = 5
 
 var starSheet pixel.Picture = loadPicture("assets/star-spritesheet.png")
-var starBatch *pixel.Batch = pixel.NewBatch(&pixel.TrianglesData{}, starSheet)
-var starSize pixel.Vec = pixel.V(5, 5)
 var starPhases [STAR_MAX_PHASE + 1]pixel.Rect
-var starDistance pixel.Vec = pixel.V(90, 90)
+var starBatch = [2][STAR_MAX_PHASE]*pixel.Batch{
+	{pixel.NewBatch(&pixel.TrianglesData{}, starSheet)}, {},
+}
+
+// Adjust star distance to fit screen
+var starDistance = pixel.Vec{
+	X: winsize.X / math.Floor(winsize.X/STAR_DISTANCE),
+	Y: winsize.Y / math.Floor(winsize.Y/STAR_DISTANCE),
+}
+
 var stars []star = make([]star, 0, 128)
 
 type star struct {
@@ -23,34 +34,17 @@ type star struct {
 
 func loadStarPhases() {
 	for i := 0; i <= STAR_MAX_PHASE; i++ {
-		phase := float64(i) * starSize.X
-		starPhases[i] = pixel.R(phase, 0, phase+starSize.X, starSize.Y)
+		phase := float64(i) * STAR_SIZE
+		starPhases[i] = pixel.R(phase, 0, phase+STAR_SIZE, STAR_SIZE)
 	}
 }
 
+// Generate stars
 func generateStars() {
-	var starNumbers pixel.Vec = pixel.V(winsize.X/starDistance.X, winsize.Y/starDistance.Y)
-	starDistance = starDistance.Add(pixel.Vec{
-		X: starNumbers.X - float64(uint8(starNumbers.X)),
-		Y: starNumbers.Y - float64(uint8(starNumbers.Y)),
-	})
-
-	// Generate stars
-	var shiftRow bool
-	var shiftAmount float64 = starDistance.X / 2
-	var renderBounds pixel.Vec = pixel.V(starNumbers.X*starDistance.X, starNumbers.Y*starDistance.Y)
-	for y := 0.0; y < renderBounds.Y; y += starDistance.Y {
-		x := 0.0
-		if shiftRow {
-			x = shiftAmount
-		}
-
-		shiftRow = !shiftRow
-		for ; x < renderBounds.X; x += starDistance.X {
-
-			//
+	for y := 0.0; math.Round(y) < winsize.Y; y += starDistance.Y {
+		for x := 0.0; math.Round(x) <= winsize.X; x += starDistance.X {
 			stars = append(stars, star{
-				pos:   pixel.V(x, y).Add(pixel.V(float64(rand.Int()%50), float64(rand.Int()%50))),
+				pos:   pixel.V(x, y).Add(pixel.V(randomAxis(STAR_RANDOMNESS), randomAxis(STAR_RANDOMNESS))),
 				phase: rand.Int() % 6,
 				shine: 1,
 			})
@@ -58,35 +52,42 @@ func generateStars() {
 	}
 }
 
-func updateStarPhase(star int) {
-	if stars[star].phase >= STAR_MAX_PHASE {
-		stars[star].shine = -1
-	} else if stars[star].phase < 1 {
-		stars[star].shine = 1
+func randomAxis(limit int) float64 {
+	return float64(rand.Int() % limit)
+}
+
+func updateStarPhases(stars []star) {
+	for i := range stars {
+		if stars[i].phase >= STAR_MAX_PHASE {
+			stars[i].shine = -1
+		} else if stars[i].phase < 1 {
+			stars[i].shine = 1
+		}
+
+		stars[i].phase += 1 * stars[i].shine
 	}
 
-	stars[star].phase += 1 * stars[star].shine
 }
 
 func updateStars() {
 	if skipFrames(2) {
-		starBatch.Clear()
+		starBatch[0][0].Clear()
+
+		if skipFrames(4) {
+			updateStarPhases(stars)
+		}
+
 		for i := 0; i < len(stars); i++ {
-
-			if skipFrames(4) {
-				updateStarPhase(i)
-			}
-
 			stars[i].pos.Y -= globalVelocity
 			if stars[i].pos.Y < 0 {
 				stars[i].pos.Y += winsize.Y
 			}
 
 			star := pixel.NewSprite(starSheet, starPhases[stars[i].phase])
-			star.Draw(starBatch, pixel.IM.Scaled(pixel.ZV, 2).Moved(stars[i].pos))
+			star.Draw(starBatch[0][0], pixel.IM.Scaled(pixel.ZV, 2).Moved(stars[i].pos))
 
 		}
 	}
 
-	starBatch.Draw(win)
+	starBatch[0][0].Draw(win)
 }
